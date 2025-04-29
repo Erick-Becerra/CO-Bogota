0fbeaba8878e33cf4403daee13d0439489e1a245
Erick-Becerra <erickbece22@gmail.com>
2025-04-29 18:56:31 -0500

    Agrego el proyecto de NetBeans VigilIA a Ingeniería-de-Software

---
+++ Ingeniería-de-Software/VigilIA/src/boton_panico/Boton_Panico.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boton_panico;
+
+/**
+ *
+ * @author l2114
+ */
+public class Boton_Panico {
+
+    VigilIA Panic = new VigilIA();
+    public void main(String[] args) {
+        Panic.setVisible(true);
+    }
+    
+}
